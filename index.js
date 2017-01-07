8b28916f2768b20ca220add878269de97abe9c21
tarukosu <u.au.sky.future12@gmail.com>
2017-01-07 16:45:48 +0900

    add code for camera streaming

--- index.js
+++ index.js
@@ -1,84 +1,57 @@
-/*
-var app = require('express').createServer();
-var io = require('socket.io').listen(app);
- 
-// カウンタ初期化
-var count = 0;
-app.listen(8124);
- 
-app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
-});
- 
-io.sockets.on('connection', function (socket) {
- 
-    // 接続時にカウンタのインクリメント
-    count++;
- 
-    // ブロードキャストで送信
-    io.sockets.emit('user connected',count);
- 
-    // 切断
-    socket.on('disconnect', function () {
- 
-        // カウンタのデクリメント
-        count--;
- 
-        // ブロードキャストで送信
-        io.sockets.emit('user disconnected',count);
-    });
-});
-*/
 var http = require("http");
 var socketio = require("socket.io");
 var fs = require("fs");
+
+/* app settings */
+var PORT = 4000;
+var TIMEOUT = 5000;
 
 var server = http.createServer(function(req, res) {
      res.writeHead(200, {"Content-Type":"text/html"});
      var output = fs.readFileSync("./index.html", "utf-8"); 
      res.end(output);
-}).listen(process.env.APP_PORT || 4000);
+}).listen(process.env.APP_PORT || PORT);
 
 var io = socketio.listen(server);
 
 var connection = 0;
-
 var to;
+
+var isStreaming = false;
+
+var startCamera = function(){
+    if(isStreaming){
+        return;
+    }
+    isStreaming = true;
+
+    // start streaming
+}
+
+var stopCamera = function(){
+    isStreaming = false;
+
+    //stop streaming
+}
 
 io.sockets.on("connection", function (socket) {
     connection ++;
+    startCamera();
     console.log("connected: " + connection);
 
     clearTimeout(to);
     to = setTimeout(function () {
         console.log("timeout");
-    }, 5000);
+        stopCamera();
+    }, TIMEOUT);
 
-    // ブロードキャストで送信
     io.sockets.emit('user connected', connection);
  
-    // 切断
     socket.on('disconnect', function () {
-         // カウンタのデクリメント
         connection--;
-         // ブロードキャストで送信
+        if(connection == 0){
+            stopCamera();
+        }
         io.sockets.emit('user disconnected', connection);
     });
-    /*
-  // メッセージ送信（送信者にも送られる）
-  socket.on("C_to_S_message", function (data) {
-    io.sockets.emit("S_to_C_message", {value:data.value});
-  });
-
-  // ブロードキャスト（送信者以外の全員に送信）
-  socket.on("C_to_S_broadcast", function (data) {
-    socket.broadcast.emit("S_to_C_message", {value:data.value});
-  });
-
-  // 切断したときに送信
-  socket.on("disconnect", function () {
-    connection --;
-//    io.sockets.emit("S_to_C_message", {value:"user disconnected"});
-  });
-  */
 });
